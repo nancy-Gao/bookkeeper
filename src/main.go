a0277a935b63a89e8dd2ca59f8b856c6cf918249
stella <337232241@qq.com>
2020-07-07 00:03:09 +0800

    feat:reader

---
+++ src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "github.com/360EntSecGroup-Skylar/excelize"
+)
+
+func main() {
+    f, err := excelize.OpenFile("Book1.xlsx")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // 获取工作表中指定单元格的值
+    cell, err := f.GetCellValue("Sheet1", "B2")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // 获取 Sheet1 上所有单元格
+    rows, err := f.GetRows("Sheet1")
+    for _, row := range rows {
+        for _, colCell := range row {
+            fmt.Print(colCell, "\t")
+        }
+        fmt.Println()
+    }
+}
